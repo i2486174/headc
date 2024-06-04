f52e3b548bbae87380c27d70f50f1fc14c36a45b
i2486174 <166976635+i2486174@users.noreply.github.com>
2024-06-04 12:49:37 +0900

    Update cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -1,15 +1,33 @@
-use std::path::PathBuf;
+use clap::{value_parser, Arg, ArgAction, Command};
 
-use clap::Parser;
-
-#[derive(Debug, Parser)]
-pub struct CliOpts {
-    #[arg(short = 'n',long, default_value = "10")]
-    pub lines: Option<u8>,
-
-    #[arg(short = 'c', long)]
-    pub bytes: Option<u8>,
-
-    #[arg(value_name = "FILE")]
-    pub files: Vec<PathBuf>,
+pub fn build_command() -> Command {
+    Command::new("headc")
+        .version("0.1.0")
+        .author("Sae Nishimura")
+        .about("Rust head")
+        .arg(
+            Arg::new("lines")
+                .short('n')
+                .long("lines")
+                .value_name("LINES")
+                .help("Number of lines")
+                .default_value("10"),
+        )
+        .arg(
+            Arg::new("bytes")
+                .short('c')
+                .long("bytes")
+                .value_name("BYTES")
+                .num_args(1)
+                .conflicts_with("lines")
+                .help("Number of bytes"),
+        )
+        .arg(
+            Arg::new("files")
+                .value_name("FILES")
+                .help("Input files(s)")
+                .action(ArgAction::Append)
+                .value_parser(value_parser!(String))
+                .default_value("-"),
+        )
 }
